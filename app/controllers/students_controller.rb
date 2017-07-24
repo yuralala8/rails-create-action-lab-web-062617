98b06d9e50522442a3b0a6d3c11d0ce7e2290097
yuralala8 <yc1435@nyu.edu>
2017-07-24 10:30:10 -0400

    rails create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,16 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+
+    redirect_to @student
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
   get '/student/:id', to: 'students#show', as: 'student'
+  post '/students/new'
+  # post '/students/new'
 end
